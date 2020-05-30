bd0d976d01a6a84b597831334ec0d534ab95196d
tanoker09 <64736798+tanoker09@users.noreply.github.com>
2020-05-30 17:43:39 +0500

    Update FactorialCalcer.java

--- dz7/src/main/java/FactorialCalcer.java
+++ dz7/src/main/java/FactorialCalcer.java
@@ -3,6 +3,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+/**
+*Класс для расчета массива факториалов
+*/
+
 public class FactorialCalcer implements Callable<List<BigInteger>>, Factoriable{
 
     String name;
